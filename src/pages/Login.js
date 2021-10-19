00e91165a33791a28b2b177652119a91020e8cc9
GustavoPD <gudalmoro@gmail.com>
2021-10-18 22:19:27 -0300

    Req 1 e 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,73 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      redirect: false,
+    };
+
+    this.handlerButton = this.handlerButton.bind(this);
+    this.handlerInput = this.handlerInput.bind(this);
+  }
+
+  handlerButton() {
+    const { email, password, redirect } = this.state;
+    console.log(email, password, redirect);
+    this.setState({
+      redirect: true,
+    });
+  }
+
+  handlerInput({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, redirect } = this.state;
+    const n = 6;
+    const validEmail = /\S+@\S+\.\S+/;
+    const showButton = validEmail.test(email) && password.length >= n
+    && email.split('').includes('@');
+    if (redirect === true) return <Redirect to="/carteira" />;
+    return (
+      <form>
+        Login
+        <label htmlFor="email">
+          <input
+            value={ email }
+            onChange={ this.handlerInput }
+            type="email"
+            name="email"
+            data-testid="email-input"
+          />
+        </label>
+        Password
+        <label htmlFor="password">
+          <input
+            value={ password }
+            onChange={ this.handlerInput }
+            type="password"
+            name="password"
+            data-testid="password-input"
+          />
+        </label>
+        <button
+          onClick={ this.handlerButton }
+          type="button"
+          data-testid="login-submit-button"
+          disabled={ !showButton }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
